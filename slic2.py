c6dea27bd4201a0c168f4a36759c40bcf107fe3a
An Mega <trieuhoangan1012@gmail.com>
2020-06-02 11:31:35 +0700

    initiate x2

---
+++ slic2.py
@@ -0,0 +1,14 @@
+import numpy as np 
+def initiate_k_center(X,k):
+    return None
+def classify_data(X,k,clusters,labels):    
+    return labels
+def update_center(X,k,cluster):
+    return None
+def distance(X,k,cluster):
+    return None
+def kmeans(X,k):
+    labels = []
+    center = []
+    clusters = (labels,center)
+    return clusters
